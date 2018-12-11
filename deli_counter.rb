fc10f90a5e7ea3de93d1111254767c217f3e4833
Ariel <arielord@buffalo.edu>
2018-12-11 20:10:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+def line(array)
+  line_size = array.size
+  counter = 1
+  if line_size == 0
+    puts "The line is currently empty."
+  else
+    while line_size > 0 do
+      puts "The line is currently: #"
+    end
+  end
+end
+
+def take_a_number(person)
+  
+end
+
+def now_serving()
+  
+end
